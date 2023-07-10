777f70c2036aa24a2f151fcb1ca3fc2ba99a3fd7
varun-Tad <varuntad141@gmail.com>
2023-07-10 17:17:27 +0530

    created commits page

---
+++ src/components/CommentsContainer.js
@@ -0,0 +1,113 @@
+import React from "react";
+
+const commentsData = [
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [
+          {
+            name: "Akshay Saini",
+            text: "Lorem ipsum dolor sit amet, consectetur adip",
+            replies: [
+              {
+                name: "Akshay Saini",
+                text: "Lorem ipsum dolor sit amet, consectetur adip",
+                replies: [
+                  {
+                    name: "Akshay Saini",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [
+                      {
+                        name: "Akshay Saini",
+                        text: "Lorem ipsum dolor sit amet, consectetur adip",
+                        replies: [],
+                      },
+                    ],
+                  },
+                  {
+                    name: "Akshay Saini",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+];
+
+const Comment = ({ data }) => {
+  const { name, text, replies } = data;
+  return (
+    <div className="flex shadow-sm bg-gray-100 p-2 rounded-lg my-2">
+      <img
+        className="w-12 h-12 rounded-lg"
+        src="https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1608/tuktukdesign160800043/61010830-user-icon-man-profile-businessman-avatar-person-glyph-vector-illustration.jpg?ver=6"
+        alt="user"
+      />
+      <div className="px-3">
+        <p className="font-bold">{name}</p>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+const CommentsList = ({ comments }) => {
+  return comments.map((comment, index) => (
+    <div key={index}>
+      <Comment data={comment} />
+      <div className="pl-5 border border-l-black ml-5">
+        <CommentsList comments={comment.replies} />
+      </div>
+    </div>
+  ));
+};
+
+const CommentsContainer = () => {
+  return (
+    <div className="m-5 p-2">
+      <h1 className="text-2xl font-bold">Comments:</h1>
+      <CommentsList comments={commentsData} />
+    </div>
+  );
+};
+
+export default CommentsContainer;
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useSidebar } from "../context/SidebarContext";
+import CommentsContainer from "./CommentsContainer";
+import LiveChat from "./LiveChat";
 
 const WatchPage = () => {
-  const { isMenuOpen, setIsMenuOpen } = useSidebar();
+  const { setIsMenuOpen } = useSidebar();
   const [searchParams] = useSearchParams();
 
   const closeSidebar = () => {
@@ -14,16 +16,24 @@ const WatchPage = () => {
     closeSidebar();
   }, []);
   return (
-    <div className="px-5">
-      <iframe
-        width="1100"
-        height="600"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+    <div className="flex flex-col">
+      <div className="px-5 flex">
+        <div>
+          <iframe
+            width="1000"
+            height="600"
+            src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div>
+          <LiveChat />
+        </div>
+      </div>
+      <CommentsContainer />
     </div>
   );
 };
